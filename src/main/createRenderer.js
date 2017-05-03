2669e9adf86108e81bbe28b768686219b726b90b
matthias.epheser <matthias.epheser@indoqa.com>
2017-05-03 16:00:06 +0200

    introduce init method

--- src/main/createRenderer.js
+++ src/main/createRenderer.js
@@ -1,18 +1,16 @@
 import {createRenderer as createFelaRenderer} from 'fela'
 import presetWeb from 'fela-preset-web'
 
-export const createRenderer = (initAppSecifics) => {
+export const createRenderer = (initAppSpecifics = () => {}) => {
   const felaRenderer = createFelaRenderer({
     plugins: [
       ...presetWeb
     ],
   })
-  
+
   felaRenderer.renderStatic(require('reset.css/reset.css'))
 
-  if (typeof initAppSecifics === 'function') {
-    initAppSecifics(felaRenderer)
-  }
-  
+  initAppSpecifics(felaRenderer)
+
   return felaRenderer
 }
