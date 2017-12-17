587fd80465b49673a6f863e37e816aca3ccaf264
Reinhard Pötz <reinhard.poetz@indoqa.com>
2017-12-17 20:16:36 +0100

    prop-types no peer dependency

--- src/main/index.js
+++ src/main/index.js
@@ -3,7 +3,7 @@ import Flex from './components/Flex.js'
 import Text from './components/Text.js'
 import {IndoqaFela} from './IndoqaFela.react.js'
 import buildTheme from './buildTheme.js'
-import withRenderer from './withRenderer.js'
+import withRenderer from './withRenderer.react.js'
 
 export {
   Box,
