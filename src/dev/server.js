7c0360831419a9e1ab0b78f00a1c9f298c62091f
Reinhard Pötz <reinhard.poetz@indoqa.com>
2018-04-16 22:22:41 +0200

    Update npm packages

--- src/dev/server.js
+++
@@ -1,4 +0,0 @@
-const options = require('../../indoqa-webpack-options.js')
-const runDevServer = require('indoqa-webpack').runDevServer // eslint-disable-line import/no-extraneous-dependencies
-
-runDevServer({options})
