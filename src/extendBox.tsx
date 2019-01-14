4bbeba294c02d40cfaf69e4fae3d1b64194950c5
Reinhard Pötz <reinhard.poetz@indoqa.com>
2019-01-14 16:23:15 +0100

    export extendBox

--- src/extendBox.tsx
+++ src/extendBox.tsx
@@ -2,6 +2,4 @@ import {IStyle} from 'fela'
 import {createComponentWithProxy} from 'react-fela'
 import {Box} from './components/Box'
 
-const extendBox = (styles: IStyle) => createComponentWithProxy(styles, Box)
-
-export default extendBox
+export const extendBox = (styles: IStyle) => createComponentWithProxy(styles, Box)
