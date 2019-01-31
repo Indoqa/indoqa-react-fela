c425eb4d107431b55436e31a1ccff3981c269c9a
Reinhard Pötz <reinhard.poetz@indoqa.com>
2019-01-31 18:24:53 +0100

    Grid: allow Grid spacing of 0

--- src/components/grid/testGridContext.tsx
+++ src/components/grid/testGridContext.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 import {Spacing} from './GridContext'
 
 export const testGridContext = (spacing: Spacing, child: React.ReactNode) => {
-  if (!spacing) {
+  if (spacing === null || spacing === undefined) {
     if (process.env.NODE_ENV !== 'production') {
       return (
         <div>Missing <span style={{color: 'white', backgroundColor: 'red'}}>GridContext</span>
