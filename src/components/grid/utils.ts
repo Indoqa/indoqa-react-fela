c8e2e8c799c067c0eab1d21f7e6f4585412e6518
Reinhard Pötz <reinhard.poetz@indoqa.com>
2019-01-31 19:07:19 +0100

    Grid: fix check for 0 unit in addUnitIfNeeded

--- src/components/grid/utils.ts
+++ src/components/grid/utils.ts
@@ -1,6 +1,6 @@
 export const addUnitIfNeeded = (value: any | string, propertyUnit?: string): string => {
   const valueType = typeof value
-  if ((valueType === 'number' || (valueType === 'string' && !isNaN(value))) && value !== 0) {
+  if ((valueType === 'number' || (valueType === 'string' && !isNaN(value)))) {
     return `${value}${propertyUnit || 'px'}`
   }
   return `${value}`
